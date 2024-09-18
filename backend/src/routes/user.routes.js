40a0c44a996b707151f96ec04a1554c8ef694f7a
Akash <thakare.akash007@gmail.com>
2024-09-19 00:35:59 +0530

    set generate-otp and verify-otp routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,19 +1,30 @@
 import { Router } from "express";
-import { forgetPassword, logingUser, logOutUser, registerUser, resetPassword, verifyOtp } from "../controllers/user.controller.js";
+import {
+  forgetPassword,
+  generateEmailOtp,
+  logingUser,
+  logOutUser,
+  registerUser,
+  resetPassword,
+  verifyEmailOtp,
+  verifyOtp,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-
 
 const routes = Router();
 
 // API endpoints for User management
-routes.route('/register').post(registerUser)
+routes.route("/register").post(registerUser);
 
-routes.route('/logIn').post(logingUser);
+routes.route("/logIn").post(logingUser);
 
-routes.route('/logout').post(verifyJWT,logOutUser)
+routes.route("/logout").post(verifyJWT, logOutUser);
 
-routes.route('/forgetPassword').post(forgetPassword)
-routes.route('/verifyOtp').post( verifyOtp)
-routes.route('/resetPassword').post( resetPassword)
+routes.route("/forgetPassword").post(forgetPassword);
+routes.route("/verifyOtp").post(verifyOtp);
+routes.route("/resetPassword").post(resetPassword);
 
-export default routes
+routes.route("/generate-otp").post(verifyJWT, generateEmailOtp);
+routes.route("/verify-otp").post(verifyJWT, verifyEmailOtp);
+
+export default routes;
