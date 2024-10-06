2cebc103bf7efd6eaf3caef82be3db84472c1318
Akash <thakare.akash007@gmail.com>
2024-10-06 22:54:16 +0530

    logout

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -141,7 +141,7 @@ const logingUser = asyncHandler(async (req, res) => {
 
 //LogOut User
 const logOutUser = asyncHandler(async (req, res) => {
-  console.log(req.user);
+  console.log(req);
   
   if (!req.user || !req.user._id) {
     return res.status(401).json(new ApiError(401, "Unauthorized request"));
